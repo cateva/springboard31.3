23a62a7885a461cda7f98958b0d08617e84e7144
LiYuanzhi <liyuanzhi@LiYuanzhideMBP.attlocal.net>
2022-07-28 20:20:02 -0700

    first commit

---
+++ part1/app.js
@@ -0,0 +1,41 @@
+let favNumber = 0;
+let baseURL = "http://numbersapi.com";
+
+//1
+
+async function part1(){
+    let data = await $.getJSON(`${baseURL}/${favNumber}?json`);
+    console.log(data);
+}
+part1();
+
+
+//2
+
+const favNumbers = [2,3,4];
+async function part2() {
+    let data = await $.getJSON(`${baseURL}/${favNumbers}?jon`);
+    console.log(data)
+}
+
+part2();
+
+//3
+async function part3() {
+    let facts = await Promise.all(
+        Array.from({ length: 4 }, () => $.getJSON(`${baseURL}/${favNumber}?json`))
+    );
+    facts.forEach(data => {
+        $('body').append(`<p>${data.text}</p>`);
+    });
+}
+
+// async function part3() {
+//     let facts = await Promise.all(
+//       Array.from({ length: 4 }, () => $.getJSON(`${baseURL}/${favNumber}?json`))
+//     );
+//     facts.forEach(data => {
+//       $('body').append(`<p>${data.text}</p>`);
+//     });
+//   }
+part3();
